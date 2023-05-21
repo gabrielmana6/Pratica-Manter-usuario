f77b54e0327838d4c11aa7a0ad958ec9fa322e86
Gabriel Manassés <gabrielmanasses13@gmail.com>
2023-05-21 15:01:01 -0300

    atividade

---
+++ primeiro-angular-alunos-4-usuarioservice/src/app/usuario/mantem-usuario/mantem-usuario.component.ts
@@ -0,0 +1,59 @@
+import { UsuarioService } from './../../shared/services/usuario.service';
+import { Component, OnInit } from '@angular/core';
+import {Usuario} from '../../shared/modelo/usuario';
+import {ActivatedRoute, Router} from '@angular/router';
+
+@Component({
+  selector: 'app-mantem-usuario',
+  templateUrl: './mantem-usuario.component.html',
+  styleUrls: ['./mantem-usuario.component.css']
+})
+export class MantemUsuarioComponent implements OnInit{
+
+  usuarioDeManutencao: Usuario;
+  estahCadastrando = true;
+  nomeBotaoManutencao = 'Cadastrar';
+
+  usuarios: Usuario[] = [];
+
+  constructor(private rotaAtual: ActivatedRoute, private roteador: Router, private usuarioService: UsuarioService) {
+    this.usuarioDeManutencao = new Usuario();
+  }
+
+  ngOnInit(): void {
+    this.usuarioService.listar().subscribe(
+      usuariosRetornados =>
+        this.usuarios = usuariosRetornados
+    );
+
+    const idParaEdicao = this.rotaAtual.snapshot.paramMap.get('id');
+    if (idParaEdicao) {
+      this.usuarioService.pesquisarPorId(parseInt(idParaEdicao)).subscribe(
+        usuarioPesquisado=> {
+          this.estahCadastrando = false;
+          this.nomeBotaoManutencao = 'Salvar';
+          this.usuarioDeManutencao = usuarioPesquisado;
+        }
+      )
+    } else {
+      this.nomeBotaoManutencao = 'Cadastrar';
+    }
+  }
+
+  manter(): void {
+    if (this.estahCadastrando && this.usuarioDeManutencao) {
+      this.usuarioService.inserir(this.usuarioDeManutencao).subscribe(
+        novoUsuario =>
+        this.usuarios.push(novoUsuario)
+    )} else {
+      this.usuarioService.atualizar(this.usuarioDeManutencao).subscribe(
+        usuario =>
+        usuario = this.usuarioDeManutencao
+      )
+    }
+
+    this.usuarioDeManutencao = new Usuario();
+    this.nomeBotaoManutencao = 'Cadastrar';
+    this.roteador.navigate(['listagemusuarios']);
+  }
+}
